9c128759cdf0dc381ed690530c035905011d430f
Luma Souza <luma.souza@sptech.school>
2024-10-27 16:56:12 -0300

    ajuste endpoint

--- src/main/java/com/example/CalencareApi/CalencareApiApplication.java
+++ src/main/java/com/example/CalencareApi/CalencareApiApplication.java
@@ -29,8 +29,8 @@ public class CalencareApiApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:3000")
-						.allowedMethods("GET", "POST", "PUT", "DELETE");
+						.allowedOrigins("http://44.198.165.164", "http://localhost:3000")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
 			}
 		};
 	}
